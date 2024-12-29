ea4b398fb61a8470996bfd67019ce058ba1e5511
Hammed <servertstng@gmail.com>
2024-12-29 17:09:05 +0100

    Commit TypeOf 1

--- src/auth/login.tsx
+++ src/auth/login.tsx
@@ -70,15 +70,16 @@ function Login()
                         }
                         appState.setUser(credentials)
                         appState.setPassport(res?.data?.data?.passport)
-                        console.log("++++++++++++")
-                        console.log(typeof res?.data?.data?.first_timer)
-                        console.log("++++++++++++")
-                        setIsLoading(false) 
                         if(res?.data?.data?.first_timer === "1")
                         {
                            navigate('/passport')
                         } else {
                            appState.setAllow('loggedIn')
+                           
+                        console.log("++++++++++++")
+                        console.log(typeof res?.data?.data?.first_timer)
+                        console.log("++++++++++++")
+                        setIsLoading(false) 
                            navigate('/')
                         }
                      } else {
